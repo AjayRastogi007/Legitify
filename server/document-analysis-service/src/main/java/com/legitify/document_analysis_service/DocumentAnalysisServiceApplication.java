86ffc1ea79d493ee0e9ba33ad64e0a3b9925cd21
AjayRastogi007 <ajayrastogi2006@gmail.com>
2025-12-19 11:00:55 +0530

    enbale caching

--- server/document-analysis-service/src/main/java/com/legitify/document_analysis_service/DocumentAnalysisServiceApplication.java
+++ server/document-analysis-service/src/main/java/com/legitify/document_analysis_service/DocumentAnalysisServiceApplication.java
@@ -2,8 +2,10 @@ package com.legitify.document_analysis_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DocumentAnalysisServiceApplication {
 
 	public static void main(String[] args) {
