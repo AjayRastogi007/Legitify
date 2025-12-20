c18f5237f2022cdb8492eb85e9aa3f6e6b3592d4
AjayRastogi007 <ajayrastogi2006@gmail.com>
2025-12-20 12:40:43 +0530

    added proper cors config

--- server/api-gateway/src/main/java/com/legitify/api_gateway/config/CorsGlobalConfig.java
+++ server/api-gateway/src/main/java/com/legitify/api_gateway/config/CorsGlobalConfig.java
@@ -1,12 +1,12 @@
 package com.legitify.api_gateway.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @Configuration
 public class CorsGlobalConfig {
@@ -14,7 +14,7 @@ public class CorsGlobalConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOriginPattern("https://*.vercel.app");
+        config.addAllowedOriginPattern("https://legitify-web.vercel.app");
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowCredentials(true);
