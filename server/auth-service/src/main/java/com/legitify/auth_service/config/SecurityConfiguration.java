701c51e9252ff59c3be62f0b3c806236c43a2827
AjayRastogi007 <ajayrastogi2006@gmail.com>
2026-04-05 08:23:32 +0530

    allow public access to auth endpoints with security configuration

---
+++ server/auth-service/src/main/java/com/legitify/auth_service/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.legitify.auth_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/legitify/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
